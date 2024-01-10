2281b00d9743f696044fac9dc04c7d8112e4450a
neo1202 <neowuhuayo@gmail.com>
2024-01-11 02:49:32 +0800

    [fix]tailwind config, homepage layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,95 @@
 import { useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import Alert from "./pages/Alert";
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertClassName, setAlertClassName] = useState("hidden");
+
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setJwtToken("");
+    navigate("/login");
+  };
+
   return (
-    <>
-      <div className="bg-emerald-300/[.2]  py-5 sm:py-10 ">H!!!ello world!</div>
-    </>
+    <div className="flex flex-col w-screen h-screen m-0 bg-slate-200">
+      <div className="flex flex-row w-full h-18">
+        <div className="w-5/6 mt-3 text-4xl text-neutral-950">
+          Go Watch a Movie!
+        </div>
+
+        <div className="flex flex-col w-1/6 text-end">
+          {jwtToken === "" ? (
+            <Link to="/login">
+              <span className="text-white bg-green-500">Login</span>
+            </Link>
+          ) : (
+            <a href="#!" onClick={logOut}>
+              <span className="text-white bg-red-500">Logout</span>
+            </a>
+          )}
+        </div>
+        <hr className="mb-12"></hr>
+      </div>
+      <hr className="h-0.5 bg-cyan-950"></hr>
+      <div className="flex flex-row flex-1 w-full">
+        <div className="flex flex-row w-1/6">
+          <nav className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+            <Link to="/" className="w-full px-4 py-2 border-b border-gray-200 ">
+              Home
+            </Link>
+            <Link
+              to="/movies"
+              className="w-full px-4 py-2 border-b border-gray-200"
+            >
+              Movies
+            </Link>
+            <Link
+              to="/genres"
+              className="w-full px-4 py-2 border-b border-gray-200"
+            >
+              Genres
+            </Link>
+            {jwtToken !== "" && (
+              <>
+                <Link
+                  to="/admin/movie/0"
+                  className="w-full px-4 py-2 border-b border-gray-200"
+                >
+                  Add Movie
+                </Link>
+                <Link
+                  to="/manage-catalogue"
+                  className="w-full px-4 py-2 border-b border-gray-200"
+                >
+                  Manage Catalogue
+                </Link>
+                <Link
+                  to="/graphql"
+                  className="w-full px-4 py-2 border-b border-gray-200"
+                >
+                  GraphQL
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
+        <div className="flex flex-col w-5/6">
+          <Alert message={alertMessage} className={alertClassName} />
+          <Outlet
+            context={{
+              jwtToken,
+              setJwtToken,
+              setAlertClassName,
+              setAlertMessage,
+            }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Ticket from "../assets/movie_tickets.jpg";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="text-center text-black">
+        <div>Find a movie to watch tonight!</div>
+        <hr />
+        <Link to="/movies">
+          <img src={Ticket} alt="movie tickets"></img>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Home;
