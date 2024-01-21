14b4350d964e5ba94fe8556adf3d67a42501967f
neo1202 <neowuhuayo@gmail.com>
2024-01-21 16:47:12 +0800

    [fix] production CORS added

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -11,8 +11,8 @@ func (app *application) routes() http.Handler {
 	// create a router mux
 	mux := chi.NewRouter()
 
-	mux.Use(middleware.Recoverer)
-
+	mux.Use(middleware.Recoverer) //如果炸了不會讓你整個app炸回home會有backup
+	mux.Use(app.enableCORS)
 	mux.Get("/", app.Home)
 
 	mux.Get("/movies", app.AllMovies)
