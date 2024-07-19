d64953d57880cca9079ba5cc90a963c9b2698f87
neo1202 <neowuhuayo@gmail.com>
2024-07-19 16:22:54 +0800

    [trivial]vite config api proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": "http://localhost:8080",
-    },
-    // hmr: {
-    //     overlay: false,
+    // proxy: {
+    //   "/api": "http://localhost:8080",
     // },
+    proxy: {
+      // 代理所有以 /api 开头的请求
+      "/api": {
+        target: "http://localhost:8080", // 你的后端服务器地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [react()],
 });
